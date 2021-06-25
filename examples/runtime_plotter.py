672a139226541c284003940dd62ac3aea28cbb1f
Aashis Khanal <sraashis@gmail.com>
2021-06-25 17:35:17 +0545

    Runtime experiment.

--- examples/runtime_plotter.py
+++ examples/runtime_plotter.py
@@ -17,20 +17,29 @@ if len(sys.argv) > 1:
 
 skip = 5
 data = []
+data_cumu = []
 for p in paths:
     if not p.endswith('.json'):
         p = p + os.sep + 'experiment' + os.sep + 'experiment_log.json'
         
     j = json.load(open(p))
-    data.append(np.cumsum(j['batch_run_time'][skip:]))
+    data.append(j['batch_run_time'][skip:])
+    data_cumu.append(np.cumsum(j['batch_run_time'][skip:]))
 data = np.array(data).T
+data_cumu = np.array(data_cumu).T
 
 colors = ['green', 'magenta', 'red']
 header = ['Single process', 'DDP', 'DAD']
 df = pd.DataFrame(data=data, columns=header)[skip:]
+df.plot(color=colors)
+plt.ylabel('Millis')
+plt.xlabel('Iteration')
+plt.savefig('runtime-compare.png')
+plt.close('all')
 
+df = pd.DataFrame(data=data_cumu, columns=header)[skip:]
 df.plot(color=colors)
 plt.ylabel('Cumulative Millis')
 plt.xlabel('Iteration')
-plt.savefig('runtime-compare.png')
+plt.savefig('runtime-compare_cumulative.png')
 plt.close('all')
