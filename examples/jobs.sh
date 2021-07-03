65d0e75fa2e883b16fc807f24fc7abdf03aee6d7
Aashis Khanal <sraashis@gmail.com>
2021-07-03 08:09:54 +0545

    Gloo backend and profiler update.

--- examples/jobs.sh
+++ examples/jobs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #python3 MNIST_dadtorch.py -ph train -seed 1 -f true -seed-all True -log net_logs_ONE -e 51
 python3 MNIST_dadtorch.py -ph train -ddp True -seed 1 -f true -seed-all True --dad-reduction False -log net_logs_DDP -e 51 --node-rank 0 --world-size 4 --dist-backend gloo
-python3 MNIST_dadtorch.py -ph train -ddp True -seed 1 -f true -seed-all True --dad-reduction True -log net_logs_DAD_AG -e 51 --node-rank 0 --world-size 4 --dist-backend gloo --comm-mode ag --ignore-backwards False
-python3 MNIST_dadtorch.py -ph train -ddp True -seed 1 -f true -seed-all True --dad-reduction True -log net_logs_DAD_AG-IB -e 51 --node-rank 0 --world-size 4 --dist-backend gloo --comm-mode ag --ignore-backwards True
-python3 MNIST_dadtorch.py -ph train -ddp True -seed 1 -f true -seed-all True --dad-reduction True -log net_logs_DAD_BC -e 51 --node-rank 0 --world-size 4 --dist-backend gloo --comm-mode bc --ignore-backwards False
-python3 MNIST_dadtorch.py -ph train -ddp True -seed 1 -f true -seed-all True --dad-reduction True -log net_logs_DAD_BC-IB -e 51 --node-rank 0 --world-size 4 --dist-backend gloo --comm-mode bc --ignore-backwards True
+python3 MNIST_dadtorch.py -ph train -ddp True -seed 1 -f true -seed-all True --dad-reduction True -log net_logs_DAD_AG -e 51 --node-rank 0 --world-size 4 --dist-backend gloo --comm-mode ag --ignore-backward False
+python3 MNIST_dadtorch.py -ph train -ddp True -seed 1 -f true -seed-all True --dad-reduction True -log net_logs_DAD_AG-IB -e 51 --node-rank 0 --world-size 4 --dist-backend gloo --comm-mode ag --ignore-backward True
+python3 MNIST_dadtorch.py -ph train -ddp True -seed 1 -f true -seed-all True --dad-reduction True -log net_logs_DAD_BC -e 51 --node-rank 0 --world-size 4 --dist-backend gloo --comm-mode bc --ignore-backward False
+python3 MNIST_dadtorch.py -ph train -ddp True -seed 1 -f true -seed-all True --dad-reduction True -log net_logs_DAD_BC-IB -e 51 --node-rank 0 --world-size 4 --dist-backend gloo --comm-mode bc --ignore-backward True
 
 
