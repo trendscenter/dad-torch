e329f1da30792d497dba0a6abae4f4b4e3b20036
Aashis Khanal <sraashis@gmail.com>
2021-07-06 23:10:01 +0545

    Remove timer from utils.

--- examples/runtime_plotter.py
+++ examples/runtime_plotter.py
@@ -8,11 +8,12 @@ import sys
 import os
 import glob
 
-paths = []
-if len(sys.argv) > 1:
-    paths = sys.argv[1:]
-else:
-    raise Exception('Must pass log directories as runtime args.')
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-paths', '--paths', nargs='*', type=str, help='Root path to Logs.')
+ap.add_argument('-keys', '--keys', nargs='*', type=str, default=[], help='Keys to plot.')
+args = vars(ap.parse_args())
 
 
 def get_log(pth):
@@ -21,20 +22,29 @@ def get_log(pth):
     return json.load(open(_jsn_pth))
 
 
-keys = [k for k in get_log(paths[0]).keys() if '_duration' in k]
-header = [p.split(os.sep)[-1].split('_')[-1] for p in paths]
+keys = args['keys']
+if len(keys) == 0:
+    _keys = [k for k in get_log(args['paths'][0]).keys() if '_duration' in k]
+    keys = [k for k in _keys if not k.startswith('l')]  # SKIP Layer
+header = [p.split(os.sep)[-1].split('_')[-1] for p in args['paths']]
 
 skip = 5
 for k in keys:
     print(f"Working on key {k}...")
     data_cumu = []
-    for p in paths:
-        data_cumu.append(np.cumsum(get_log(p).get(k, [])[skip:]))
+    for p in args['paths']:
+        try:
+            data_cumu.append(np.cumsum(get_log(p)[k][skip:]))
+        except Exception as e:
+            print(f" ***** Error loading path {p} : {e} ***** ")
 
-    data_cumu = np.array(data_cumu).T
-    df = pd.DataFrame(data=data_cumu, columns=header)[skip:]
-    df.plot()
-    plt.ylabel('Cumulative Millis')
-    plt.xlabel('Iteration')
-    plt.savefig(f'{k}_cumulative.png')
-    plt.close('all')
+    try:
+        data_cumu = np.array(data_cumu).T
+        df = pd.DataFrame(data=data_cumu, columns=header)[skip:]
+        df.plot()
+        plt.ylabel('Cumulative Millis')
+        plt.xlabel('Iteration')
+        plt.savefig(f'{k}_cumulative.png')
+        plt.close('all')
+    except Exception as e:
+        print(f'***** Key: {k}| Error in data: {e} *****')
