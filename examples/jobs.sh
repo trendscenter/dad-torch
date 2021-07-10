44111f7ab736304037a176bd3cb9d4d4e985af7a
Aashis Khanal <sraashis@gmail.com>
2021-07-10 13:42:47 +0545

    Rank DAD added. Decoupled DADParallel.

--- examples/jobs.sh
+++ examples/jobs.sh
@@ -8,9 +8,9 @@ BACKEND="nccl"
 EPOCH=51
 for b in "${BATCH_SIZES[@]}"
 do
-  python3 MNIST_dadtorch.py -ddp True -ph $PHASE -nw 4 -e 51 --dist-backend nccl -b b -log net_logs_dSGD"$b" --dad-reduction base
-  python3 MNIST_dadtorch.py -ddp True -ph $PHASE -nw 4 -e 51 --dist-backend nccl -b b -log net_logs_DAD"$b" --dad-reduction dad
-  python3 MNIST_dadtorch.py -ddp True -ph $PHASE -nw 4 -e 51--dist-backend nccl -b b -log net_logs_DAD-IB"$b" --dad-reduction dad --ignore-backward True --world-size 4
+  python3 MNIST_dadtorch.py -ddp True -ph $PHASE -nw 4 -e $EPOCH --dist-backend nccl -b $b -log net_logs_dSGD"$b" --dad-reduction base
+  python3 MNIST_dadtorch.py -ddp True -ph $PHASE -nw 4 -e $EPOCH --dist-backend nccl -b $b -log net_logs_DAD"$b" --dad-reduction dad
+  python3 MNIST_dadtorch.py -ddp True -ph $PHASE -nw 4 -e $EPOCH --dist-backend nccl -b $b -log net_logs_DAD-IB"$b" --dad-reduction dad --ignore-backward True
   python3 runtime_plotter.py -paths net_logs_dSGD"$b" net_logs_DAD"$b" net_logs_DAD-IB"$b" -keys batch_duration -name $b
 done
 
