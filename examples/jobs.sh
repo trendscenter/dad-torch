6bd338c140c545d50021398a04029cd2a25b74d1
Aashis Khanal <sraashis@gmail.com>
2021-09-10 22:42:21 -0400

    RUns

--- examples/jobs.sh
+++ examples/jobs.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-NUM_SITES=(2)
-BATCH_SIZES=(16 64 128)
+BATCH_SIZES=(64)
 
 PHASE="train"
-BACKEND="nccl"
+BACKEND="gloo"
 
-EPOCH=51
-
-#python MNIST_dadtorch.py -ddp True -ph train -nw 4 --dist-backend nccl -b 64 -log net_logs --dad-reduction dad
+EPOCH=5
 for b in "${BATCH_SIZES[@]}"
 do
-  python3 MNIST_dadtorch.py -ddp True -ph $PHASE -nw 4 -e $EPOCH --dist-backend nccl -b $b -log net_logs_dSGD"$b" --dad-reduction base
-  python3 MNIST_dadtorch.py -ddp True -ph $PHASE -nw 4 -e $EPOCH --dist-backend nccl -b $b -log net_logs_DAD"$b" --dad-reduction dad
-  python3 MNIST_dadtorch.py -ddp True -ph $PHASE -nw 4 -e $EPOCH --dist-backend nccl -b $b -log net_logs_DAD-IB"$b" --dad-reduction dad --ignore-backward True
-  python3 runtime_plotter.py -paths net_logs_dSGD"$b" net_logs_DAD"$b" net_logs_DAD-IB"$b" -keys batch_duration -name $b
+  python3 MNIST_dadtorch.py -ddp True -ph $PHASE -nw 4 -e $EPOCH --dist-backend $BACKEND -b $b -log net_logs_dSGD"$b" --dad-reduction base --world-size 4
+  python3 MNIST_dadtorch.py -ddp True -ph $PHASE -nw 4 -e $EPOCH --dist-backend $BACKEND -b $b -log net_logs_DAD"$b" --dad-reduction dad --world-size 4
+  python3 runtime_plotter.py -paths net_logs_dSGD"$b" net_logs_DAD"$b" -keys batch_duration -name $b
 done
 
