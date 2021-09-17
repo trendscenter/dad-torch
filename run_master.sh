357c2c1790ad3ddeba197f0463b4b2d6f6a9c716
Aashis Khanal <sraashis@gmail.com>
2021-09-17 00:17:54 -0400

    slurm with recursive modules.

--- run_master.sh
+++ run_master.sh
@@ -7,8 +7,7 @@
 #SBATCH --gpus 1
 #SBATCH -A PSYC0002
 #SBATCH --oversubscribe
-#SBATCH --exclude trendsagn001.rs.gsu.edu,trendsagn002.rs.gsu.edu,trendsagn003.rs.gsu.edu,trendsagn004.rs.gsu.edu,trendsagn005.rs.gsu.edu,trendsagn006.rs.gsu.edu,trendsagn007.rs.gsu.edu,trendsagn008.rs.gsu.edu,trendsagn009.rs.gsu.edu,trendsagn011.rs.gsu.edu,trendsagn010.rs.gsu.edu,trendsagn016.rs.gsu.edu,trendsagn017.rs.gsu.edu,trendsagn018.rs.gsu.edu,trendsagn020.rs.gsu.edu
-
+##SBATCH --exclude trendsagn001.rs.gsu.edu,trendsagn002.rs.gsu.edu,trendsagn003.rs.gsu.edu,trendsagn004.rs.gsu.edu,trendsagn005.rs.gsu.edu,trendsagn006.rs.gsu.edu,trendsagn011.rs.gsu.edu,trendsagn012.rs.gsu.edu,trendsagn013.rs.gsu.edu,trendsagn014.rs.gsu.edu,trendsagn015.rs.gsu.edu,trendsagn016.rs.gsu.edu,trendsagn017.rs.gsu.edu,trendsagn018.rs.gsu.edu,trendsagn020.rs.gsu.edu
 eval "$(conda shell.bash hook)"
 
 cd /home/users/akhanal1/TrendsLab/dad-torch/
@@ -30,4 +29,4 @@ else
 fi
 
 cd examples
-python MNIST_dadtorch.py -ddp True --node-rank 0 --num-nodes $sites --dad-reduction $mode --dist-url tcp://10.245.12.98:8998 --master-addr 10.245.12.98 --master-port 8998 -ph train --dist-backend nccl --batch_size 64 -log "net_logs/"$branch"/"$mode"-DADs"$sites
+python MNIST_dadtorch.py -ddp True --node-rank 0 --num-nodes $sites --dad-reduction $mode --dist-url tcp://10.245.12.102:8998 --master-addr 10.245.12.102 --master-port 8998 -ph train --dist-backend nccl --batch_size 64 -log "net_logs/"$branch"/"$mode"-DADs"$sites
